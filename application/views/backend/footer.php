300e6af6e7821d1630619413ae3650a007221ebd
jossarvy <jonathankuembe91@gmail.com>
2022-09-07 09:10:24 +0100

    Initial commit

---
+++ application/views/backend/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="main">
+	
+    
+    <center  style="color:green">
+    &copy; <?php echo date('Y')?> | School Management System for INASEL Absl used for the Management of COOMPLEXE SCOLAIRE ELANDO | <br>
+        <strong > Developed by Jonathan NKUEMBE Freelancer Fullstuck</strong>
+        <p>Contact: +243 89 930 80 17</p>
+    </center>
+   
+    <div>
+
+    </div>
+</footer>
